000eb149b873c8bfc993b37f38c9c5173115c14f
Kai Marshland <kaimarshland@gmail.com>
2024-04-10 15:41:32 -0700

    Try supporting postgres 16 pt 4

--- install-pg-dump.sh
+++ install-pg-dump.sh
@@ -13,13 +13,19 @@ if [[ "$POSTGRES_VERSION" != @(11|12|13|14|15|16) ]]; then
   exit 1
 fi
 
+# if POSTGRES_VERSION is 16:
+if [[ "$POSTGRES_VERSION" == 16 ]]; then
+  sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+  sudo dnf install -y postgresql16
+  exit 0
+fi
+
 cat <<EOF > /etc/yum.repos.d/pgdg.repo
 [pgdg$POSTGRES_VERSION]
 name=PostgreSQL $POSTGRES_VERSION for RHEL/CentOS 7 - x86_64
-baseurl=https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/
+baseurl=http://download.postgresql.org/pub/repos/yum/$POSTGRES_VERSION/redhat/rhel-7-x86_64
 enabled=1
-gpgcheck=1
-gpgkey=https://download.postgresql.org/pub/repos/yum/RPM-GPG-KEY-PGDG
+gpgcheck=0
 EOF
 
 yum install postgresql$POSTGRES_VERSION-server -y
